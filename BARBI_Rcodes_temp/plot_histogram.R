042dc9abd89cfad3ae610aab63e36b4f2c3650dd
Pratheepa Jeganathan <pjeganat@stanford.edu>
2019-04-04 11:35:53 -0700

    remove temp R codes

--- BARBI_Rcodes_temp/plot_histogram.R
+++
@@ -1,6 +0,0 @@
-taxa <- 18
-dd <- data.frame(real=taxa_post[[taxa]][-(1:burnIn),], cont=rgamma((itera-burnIn+1), shape= gammPrior[[sam]][[1]][taxa], rate = gammPrior[[sam]][[2]][taxa]))
-ddl <- gather(dd)
-ggplot(ddl, aes(x=value, col = key,fill=key))+ geom_density()+
-        geom_vline(xintercept = as.numeric(gammPrior[[sam]][[3]][taxa]))
-
