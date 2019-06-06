47da18ab56f40e423de977d59e7e49c968f04785
Pratheepa Jeganathan <pjeganat@stanford.edu>
2019-06-06 12:12:58 -0700

    renmae files

--- BARBI_LDA/MLE_of_dirichlet_distribution.R
+++
@@ -1,8 +0,0 @@
-set.seed(789)
-N <- 5
-probs <- c(.5, .3, .2 )
-alpha0 <- .5
-alpha <- alpha0*probs
-alpha <- matrix( alpha, nrow=N, ncol=length(alpha), byrow=TRUE)
-x <- sirt::dirichlet.simul(alpha)
-dirichlet.mle(x)
